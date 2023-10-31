ecbc06267f59f02a8ade3c2679ac23bcac716d23
Li zhuoqun <m15004059308@163.com>
2023-10-31 12:23:40 +0800

    add: add lab2.

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,13 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesSimple1()
+    {
+        IntList lst = IntList.of(14, 15, 16, 17, 18, 17, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 289 -> 18 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
 }
